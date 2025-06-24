c55d326cbf479451a231e1ecd2ba5853bcb5bff6
abhishetty21 <abhishek.21ad001@sode-edu.in>
2025-06-24 17:05:10 +0530

    models changed

--- order_backend/app/main.py
+++ order_backend/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="Order Management System")
 # CORS Configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React frontend origin
+    allow_origins=["http://localhost:3000"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
